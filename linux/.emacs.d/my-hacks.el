f1cd8424e5830328ba386ae63a3c66336ac5cffb
Craig Vyvial <cp16net@gmail.com>
2015-02-04 20:56:53 -0600

    adding some Pymacs to the mix with auto-complete

--- linux/.emacs.d/my-hacks.el
+++ linux/.emacs.d/my-hacks.el
@@ -7,9 +7,35 @@
 (global-yalinum-mode t)
 
 ;; auto complete enabled
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(ac-config-default)
+;; (require 'auto-complete-config)
+;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+;; (ac-config-default)
+
+(defmacro after (mode &rest body)
+  `(eval-after-load ,mode
+     '(progn ,@body)))
+
+(after 'auto-complete
+       (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+       (setq ac-use-menu-map t)
+       (define-key ac-menu-map "\C-n" 'ac-next)
+       (define-key ac-menu-map "\C-p" 'ac-previous))
+
+(after 'auto-complete-config
+       (ac-config-default)
+       (when (file-exists-p (expand-file-name "/Users/craig.vyvial/.emacs.d/sites-lisp/Pymacs"))
+         (ac-ropemacs-initialize)
+         (ac-ropemacs-setup)))
+
+(after 'auto-complete-autoloads
+       (autoload 'auto-complete-mode "auto-complete" "enable auto-complete-mode" t nil)
+       (add-hook 'python-mode-hook
+                 (lambda ()
+                   (require 'auto-complete-config)
+                   (add-to-list 'ac-sources 'ac-source-ropemacs)
+                   (auto-complete-mode))))
+
+
 
 (require 'fill-column-indicator)
 (setq fci-rule-column 80)
