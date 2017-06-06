2bd0d019a200e9652d03baf9fcdc073c0140dfbf
Craig Vyvial <cp16net@gmail.com>
2017-06-06 08:39:08 -0500

    adding some stuff to the init.el (auto-generated)

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -6,3 +6,17 @@
 ;;(package-initialize)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet yaml-mode workgroups2 web-mode traad smooth-scrolling ox-pandoc nyan-mode neotree multiple-cursors monokai-theme magit kotlin-mode js2-mode helm-projectile go-guru go-eldoc go-autocomplete flymake-go flycheck-pyflakes exec-path-from-shell dockerfile-mode diff-hl company-jedi company-anaconda))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
