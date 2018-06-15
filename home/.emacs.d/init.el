1693a21aa5611d19fcc10f68c2a3db1cabe1f6c2
Craig Vyvial <craig.vyvial@planet.com>
2018-06-15 01:40:04 -0500

    go mode is MUCH better now :)
    * added back missing autocomplete
    * added flycheck linting with gometalinter
    * remove the company stuff because i'm starting to like AC better
    * left the org categories images in the init.el for now

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -6,3 +6,18 @@
 ;;(package-initialize)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+ '(org-agenda-category-icon-alist
+   (quote
+    (("Football" "~/.emacs.d/icons/Soccer_ball.png" nil nil :ascent center)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
