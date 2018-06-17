ec9e1d529dad0934acb66cfa92607182f63e5733
Craig Vyvial <craig.vyvial@planet.com>
2018-06-17 01:44:45 -0500

    markdown preview support

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -11,6 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(markdown-command "marked")
  '(org-agenda-category-icon-alist
    (quote
     (("Football" "~/.emacs.d/icons/Soccer_ball.png" nil nil :ascent center))))
